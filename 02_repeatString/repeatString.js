fc0407599900fbc482d67730ae5091de62f66d26
Ivan Orribo <ivanorribo@gmail.com>
2024-08-13 08:47:22 +0100

    second exercise completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) {return 'ERROR'};
+    let end = '';
+    for (i = 0; i < num; i++){
+        end = end + string;
+    };
+    return end;
 };
 
 // Do not edit below this line
