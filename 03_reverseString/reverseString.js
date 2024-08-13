35fbdc651aceaa922f2aa891e59d27d8c9b68b4f
ivan orribo <68389986+ivanorribo@users.noreply.github.com>
2024-08-13 08:56:44 +0100

    third task completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let rev = '';
+    for (i = string.length-1; i >= 0; i--){
+        rev += string[i];
+    }
+    return rev;
 };
 
 // Do not edit below this line
