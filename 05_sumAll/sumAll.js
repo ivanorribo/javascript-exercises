2594bfc889d927d6684056b790214dc10ccc7561
Ivan Orribo <68389986+ivanorribo@users.noreply.github.com>
2024-09-02 08:56:50 +0100

    fith task completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+let total = 0;
 
+if (Number.isInteger(min) && Number.isInteger(max)){
+    if (min > max)  {
+        for(let i = max; max <= min; max++) {
+            total += max;
+            }
+    } else if (min < 0 || max < 0) {
+        return('ERROR');
+    } else {
+        for(let i = min; min <= max; min++) {
+        total += min;
+        }
+    }
+    return total;
+} else  {
+    return 'ERROR';
+}
 };
 
 // Do not edit below this line
